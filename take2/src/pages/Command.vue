c26edacacf88eff38298b349b5090b0ee9888b4d
nicomancer-pelican <52576822+nicomancer-pelican@users.noreply.github.com>
2020-05-20 15:47:31 +0100

    without Quasar framework

--- take2/src/pages/Command.vue
+++ take2/src/pages/Command.vue
@@ -2,10 +2,10 @@
   <header>
     Voice Command
     <div>
-      <button id="button">Start listening</button>
+      <button id='button'>Start listening</button>
     </div>
-    <div id="result"></div>
-      <p id="message" hidden aria-hidden="true">
+    <div id='result'></div>
+      <p id='message' hidden aria-hidden='true'>
         Your browser doesn't support Speech Recognition. Sorry.
       </p>
   </header>
