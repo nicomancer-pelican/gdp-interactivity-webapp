d4d41ba6fa207efaca931fa1b18bfc52b6e0c7d4
nicomancer-pelican <52576822+nicomancer-pelican@users.noreply.github.com>
2020-05-26 18:12:47 +0100

    report form implemented

---
+++ public/scripts/report.js
@@ -0,0 +1,38 @@
+window.addEventListener('DOMContentLoaded', () => {
+    // LINK BUTTONS ETC.
+    const button = document.getElementById('button')
+    const main = document.getElementsByTagName('main')[0]
+
+    // FIREBASE REALTIME DATABASE BITS AND BOBS
+    var database = firebase.database();
+    var dbRef = firebase.database().ref('reports');
+
+    // SELECT TRACK
+    button.addEventListener('click', event => {
+      var commandListRef = firebase.database().ref(dbRef)
+      var newCommandRef = commandListRef.push();
+
+      var text = document.getElementById('input').value;
+
+      newCommandRef.set({
+        'problem' : text
+      })
+      setTimeout(function () {
+        window.location.reload();
+        },1000)
+      })
+
+})
+  
+
+  
+// SIDE BAR
+/* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+  document.getElementById("mySidepanel").style.width = "250px";
+}
+
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+}
